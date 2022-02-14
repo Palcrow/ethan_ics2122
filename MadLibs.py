b49a62e22b53cd70ce13835433be8da9d1cb8c5b
Paltrove <90796726+Paltrove@users.noreply.github.com>
2022-02-14 14:56:08 -0500

    Create MadLibs.py

---
+++ MadLibs.py
@@ -0,0 +1,25 @@
+#Madlibs
+# Ethan Liberty
+# Feb 14 2022
+
+verb1 = input('Enter Verb: ')
+adjective1 = input('Enter Adjective: ')
+noun1 = input('Enter Noun(Plural): ')
+adjective2 = input('Enter Adjective: ')
+verb2 = input('Enter Verb Ending in "ing": ')
+verb3 = input('Enter Verb: ')
+number = input('Enter Number: ')
+adjective3 = input('Enter Adjective: ')
+noun2 = input('Enter Noun(Plural): ')
+noun3 = input('Enter Noun(Plural): ')
+noun4 = input('Enter Noun(Plural): ')
+relative = input('Enter Relative(Plural): ')
+adjective4 = input('Enter Adjective: ')
+adjective5 = input('Enter Adjective: ')
+noun5 = input('Enter Noun(Plural): ')
+
+print(f'''You choose Verb: {verb1}, Adjective: {adjective1}, Noun: {noun1}, Adjective: {adjective2}, Verb: {verb2},
+Verb: {verb3}, Number: {number}, Adjective: {adjective3}, Noun: {noun2}, Noun: {noun3},
+Noun: {noun4}, Relative: {relative}, Adjective: {adjective4}, Adjective: {adjective5}, Noun: {noun5}''')
+
+print(f'Come {verb1} at WALMART, where you`ll receive {adjective1} discounts on all of your favorite brand name {noun1}. Our {adjective2} and {verb2} associates are there to {verb3} you {number} hours a day. Here you will find {noun2} prices on the {noun3} you need. {noun4} for the moms, {relative} for the kids and all the latest electronics for the 1. So come on down to your 1 1 WALMART where the 1 come first.')
