a04762f64d7b15c9aa5c1dfe116515928d8df1a0
Paltrove <90796726+Paltrove@users.noreply.github.com>
2022-03-22 15:04:01 -0400

    Add files via upload

---
+++ EthanP2.py
@@ -0,0 +1,7 @@
+from EthanP2Functions import *
+
+notDone = True
+fileName = input('Input Business Name: ') + 'Staff.txt'
+
+while notDone:
+    menu()
---
+++ EthanP2Functions.py
@@ -0,0 +1,13 @@
+def menu():
+    global notDone
+    choice = input('\nWhat Do You Want To Do?\n1. Add New Staff\n2. Print First Staff Member\n3. Print Last Staff Member\n4. Print All Staff Members\n0. Quit\n\n: ')
+    if choice == '1':
+        print('working on that')
+    elif choice == '2':
+        print('working on that')
+    elif choice == '3':
+        print('working on that')
+    elif choice == '4':
+        print('working on that')
+    elif choice == '0':
+        print('working on that')
