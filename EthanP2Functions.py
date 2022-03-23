48719d1a9db9a63084d5c1595d0cf2174e3bfaf7
Paltrove <90796726+Paltrove@users.noreply.github.com>
2022-03-22 22:11:20 -0400

    Update EthanP2Functions.py

--- EthanP2Functions.py
+++ EthanP2Functions.py
@@ -1,13 +1,84 @@
+import os
+from time import sleep
+
+
 def menu():
-    global notDone
-    choice = input('\nWhat Do You Want To Do?\n1. Add New Staff\n2. Print First Staff Member\n3. Print Last Staff Member\n4. Print All Staff Members\n0. Quit\n\n: ')
-    if choice == '1':
-        print('working on that')
-    elif choice == '2':
-        print('working on that')
-    elif choice == '3':
-        print('working on that')
-    elif choice == '4':
-        print('working on that')
-    elif choice == '0':
-        print('working on that')
+    
+    notDone = True
+    while notDone:
+        os.system('cls')
+        global fileName
+        fileName = 'Adminastration.txt'
+        gui = '\nWhat Do You Wish To Do?\n'
+        gui += ' 1. Add New Teacher.\n'
+        gui += ' 2. Display Newest Working Teacher.\n'
+        gui += ' 3. Display Oldest Working Teacher.\n'
+        gui += ' 4. Display All Teachers\n'
+        gui += ' 0. Exit the program.\n'
+        print(gui)
+        choice = input(': ')
+        if choice == '1':
+            addStaff()
+        elif choice == '2':
+            newStaff()
+        elif choice == '3':
+            oldStaff()
+        elif choice == '4':
+            allStaff()
+        elif choice == '0':
+            notDone = False
+
+def addStaff():
+    loop = True
+    while loop:
+        f = open(fileName, 'a')
+        os.system('cls')
+        print('Add A Employee')
+        name = input('Name: ')
+        prof = input('Department: ')
+        exp = int(input('Length Of Employment (Number Only): '))
+        earned = exp*51263
+        os.system('cls')
+        print(f'''
+Name: {name}
+Department: {prof}
+Length Of Employment: {exp}
+Total Earnings: ${earned}
+''')
+        answer = input('Is This Information Correct\n [Yes] [No]: ')
+        if answer == 'Yes':
+            f.write(f'Name: {name}  Department: {prof}  Length Of Employment: {exp}  Earnings: ${earned}\n')
+            allDone = input('Is This All The Employees You Wish To Add\n [Yes] [No]: ')
+            if allDone == 'Yes':
+                loop = False
+                os.system('cls')
+            else:
+                os.system('cls')
+        else:
+            os.system('cls')
+    f.close()
+
+def newStaff():
+    os.system('cls')
+    f = open(fileName, 'r')
+    for x in f:
+        pass
+    last_line = x
+    print(last_line)
+    sleep(30)
+    f.close
+
+def oldStaff():
+    os.system('cls')
+    f = open(fileName, 'r')
+    print(f.readline())
+    sleep(30)
+    f.close
+
+def allStaff():
+    os.system('cls')
+    f = open(fileName, 'r')
+    for x in f:
+        print(x, end = '')
+    sleep(30)
+    f.close
