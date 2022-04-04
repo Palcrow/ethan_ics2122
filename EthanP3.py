603f1f5f4ffa9f8cc21bf44edc77506a0334ff9a
Paltrove <90796726+Paltrove@users.noreply.github.com>
2022-04-04 14:10:23 -0400

    Create EthanP3.py

---
+++ EthanP3.py
@@ -0,0 +1,19 @@
+a = int(input('Enter Value For A: '))
+if a == 0:
+    print('Cant Be 0')
+else:
+    b = int(input('Enter Value For B: '))
+    c = int(input('Enter Value For C: '))
+
+    division = 2*a
+
+    if b**2-4*a*c < 0:
+        print('No Roots')
+    elif b**2-4*a*c == 0:
+        root = -b/division
+        print(f'One Root At {root}')
+    else:
+        sr = (b**2-4*a*c)**0.5
+        pSum = -b+sr
+        nSum = -b-sr
+        print(f'One Root At {pSum/division}\nAnd One Root At {nSum/division}')
