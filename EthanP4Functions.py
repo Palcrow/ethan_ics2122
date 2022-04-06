96ad5c21942a9767af07d4b74e3d13490860f71d
Paltrove <90796726+Paltrove@users.noreply.github.com>
2022-04-06 19:41:49 -0400

    Add files via upload

---
+++ EthanP4Functions.py
@@ -0,0 +1,24 @@
+def encrypt(fileName, shifts):
+    print('yes')
+    f = open(fileName, 'r')
+    turns = int(shifts)
+    punc = {96,126,33,64,35,36,37,94,38,42,40,41,45,95,61,43,91,123,93,125,59,58,44,60,46,62,47,63,32}
+    msg = ''
+    code = ''
+    print('yes')
+    for line in f:
+        code += line
+    print('yes')
+    code = code.upper()
+    for i in code:
+        letter = ord(i)
+        if letter in punc:
+            msg += chr(letter)
+        else:
+            letter = letter + turns
+            if letter > 90:
+                remainder = letter - 90
+                letter = 65 + remainder
+            msg += chr(letter)
+    
+    print(msg)
