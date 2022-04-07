c9f1366536eda3cd83b7ab10c0662575634d63b6
Paltrove <90796726+Paltrove@users.noreply.github.com>
2022-04-07 14:59:00 -0400

    Update EthanP4.py

--- EthanP4.py
+++ EthanP4.py
@@ -1,6 +1,16 @@
 def encrypt(fileName, shifts):
-    f = open(fileName, 'r')
+    global nameFile
+    global turns
+    nameFile = fileName
     turns = int(shifts)
+    wantPunc = input('Remove Punctuation [Yes] [No]: ')
+    if wantPunc == 'Yes':
+        withPunc()
+    else:
+        noPunc()
+
+def withPunc():
+    f = open(nameFile, 'r')
     punc = {96,126,33,64,35,36,37,94,38,42,40,41,45,95,61,43,91,123,93,125,59,58,44,60,46,62,47,63,32}
     msg = ''
     code = ''
@@ -17,5 +27,23 @@ def encrypt(fileName, shifts):
                 remainder = letter - 90
                 letter = 65 + remainder
             msg += chr(letter)
-    
-    print(msg)
+
+def noPunc():
+    f = open(nameFile, 'r')
+    punc = ('`~!@#$%^&*()-_=+[{]}\|;:,<.>/?')
+    msg = ''
+    code = ''
+    for line in f:
+        code += line
+    code = code.upper()
+    for i in code:
+        if i not in punc:
+            letter = ord(i)
+            if letter == 32:
+                msg += chr(letter)
+            else:
+                letter = letter + turns
+                if letter > 90:
+                    remainder = letter - 90
+                    letter = 65 + remainder
+                msg += chr(letter)
